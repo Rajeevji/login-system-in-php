a2ce5fb019ecaa410218113be2c5d97de5968f24
Rajeev Minj <49488083+Rajeevji@users.noreply.github.com>
2019-06-26 14:54:19 +0530

    i'm make a login system in php
    i'm make a login system in php

---
+++ dashbord2.php
@@ -0,0 +1,125 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+  <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+  <link type="text/css" rel="stylesheet" href="css/main.css" />
+  <link href="https://fonts.googleapis.com/css?family=Cormorant+Unicase" rel="stylesheet">
+
+
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title title="">IT VISION</title>
+  <style>
+    footer,header,.main{
+    padding-left:300px;
+
+    }
+
+  @media(max-width:992px)
+  {
+
+    footer,header,.main{
+    padding-left:0px;
+
+    }
+  }
+  </style>
+</head>
+<body style="font-family: 'Playfair Display', serif;font-family: 'Noto Serif', serif;">
+            <div class="navbar-fixed z-depth-5">
+
+              <nav class="teal">
+                <div class="nav-wrapper">
+                  <div class="container">
+                    <a href="#" class="brand-logo center">Dashbord</a>
+                    <a href="#" class="button-collapse show-on-large" data-activates="sidenav"> <i class="material-icons">menu</i></a>
+
+                  </div>
+                  
+                </div>
+                
+              </nav>
+         </div>
+
+         <ul class="side-nav fixed" id="sidenav">
+          <li>
+            <div class="user-view">
+               <div class="background">
+              <img src="img/StockSnap_ARAGQYVAYC.jpg" alt="" class="responsive-img">
+              </div>
+              <a href=""> <img src="img/Rajeev Minj.jpg"></a>
+
+            </div>
+           
+          </li>
+           
+         </ul>
+         <div class="main">
+           
+
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis ipsum sunt, sed velit ipsa molestias, eligendi impedit laudantium ducimus at repellat, vitae placeat! Voluptatibus eveniet reprehenderit maxime. Vel fugiat accusamus expedita, quas autem officia earum repudiandae et ex blanditiis voluptatum quod id ut perspiciatis, ipsam libero maiores ad nam ab suscipit dolore! Iusto dicta sapiente qui quae consectetur minus sint est cum repudiandae rem dolorem recusandae eius nulla excepturi nesciunt eum nisi, totam ex minima facilis officiis ipsam vitae ratione. Unde distinctio, nobis, vitae officia ullam error eum qui quia in excepturi quo sit aliquam consequuntur debitis, laborum accusantium pariatur. Animi, cupiditate natus quibusdam, ipsa reprehenderit tempore! Amet quasi quae incidunt commodi. Consectetur ullam vero iste qui nobis voluptatibus recusandae deserunt ducimus unde quia eligendi, dolor doloribus sapiente repudiandae optio temporibus odio error harum beatae tenetur laudantium cupiditate. Minus quo tenetur iusto nostrum, consequatur, aspernatur totam? Consectetur magni nulla libero omnis, cumque officia iusto architecto quos, minus quo animi, alias doloribus molestiae voluptate saepe eum vitae hic corporis pariatur amet possimus eveniet. Quaerat voluptatum quo molestiae repellat quis tenetur, officia dolorem aperiam. Autem vitae delectus nam, vel tempore magnam eum placeat, ut officiis sapiente soluta ex, quas dolore distinctio natus!
+
+         </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+              <footer class="page-footer teal">
+              <div class="container">
+              Theme Developed By, Rajeev Minj
+              <span class="right">
+              Follow Us On
+              <a href="" class="btn btn-floating white"><i class="fa fa-facebook blue-text" aria-hidden="true"></i></a>
+
+              <a href="#" class="btn btn-floating white"><i class="fa fa-instagram red"></i></a>
+              <a href="#" class="btn btn-floating white"><i class="fa fa-twitter blue-text"></i></a>
+              <a href="#" class="btn btn-floating white"><i class="fa fa-youtube red" aria-hidden="true"></i></a>
+              </span> 
+              </div>
+              <div class="clearfix"></div>
+
+              <div class="footer-copyright">
+              <div class="container">
+              &copy; All Right Reserved, IT VISION
+              </div>
+              </div>
+              </footer>
+<!--Import jQuery before materialize.js-->
+<script type="text/javascript" src="js/jquery.js"></script>
+  <script type="text/javascript" src="js/materialize.min.js"></script>
+  <script>
+    $(document).ready(function () {
+      // Custom JS & jQuery here
+      $('.button-collapse').sideNav();
+    });
+
+    // Or with jQuery
+
+    $(document).ready(function(){
+    $('.slider').slider();
+  });
+        
+  </script>
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,88 @@
+<?php
+include "includes/header.php";
+if(!isset($_SESSION['username']))
+{
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="../css/materialize.min.css" media="screen,projection" />
+  <link type="text/css" rel="stylesheet" href="css/main.css" />
+  <!--Let browser know website is optimized for mobile-->
+   <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title> Php Log in page</title>
+</head>
+<body style="background-image:url('../img/img12.jpg');background-size:cover">
+<div class="row" style="margin-top:100px">
+<div class="col l6 offset-l3 m8 offset-m2 s12">
+<div class="card-panel center grey" style="margin-bottom:0px">
+<ul class="tabs grey">
+<li class="tab">
+<a href="#login" class="black-text">Login</a>
+</li>
+<li class="tab">
+<a href="#signup" class="black-text">Sign Up</a>
+</li>
+</ul>
+</div>
+
+</div>
+
+<div class="col l6 offset-l3 m8 offset-m2 s12" id="login" >
+<div class="card-panel center red lighten-3" style="margin-top:1px">
+<h5>Login To Continue</h5>
+<?php
+if(isset($_SESSION['message']))
+{
+  echo $_SESSION['message'];
+  unset($_SESSION['message']);
+}
+?>
+<form action="login_check.php" method="POST">
+<div class="input-field">
+<input type="text" id="username" name="username" placeholder="Username" required="required" style="border-radius:20px;">
+</div>
+<div class="input-field">
+<input type="password" name="password" placeholder="Password" required="required"  style="border-radius:20px;">
+</div>
+<input type="submit" name="login" value="Login" class="btn">
+</form>
+</div>
+</div>
+<div class="col l6 offset-l3 m8 offset-m2 s12" id="signup">
+<div class="card-panel center blue" style="margin-top:1px">
+<h5>SignUp Now</h5>
+<form action="signup.php" method="POST">
+<div class="input-field">
+<input type="email" name="email" id="email" placeholder="Enter Email" class="validate" required=""  style="border-radius:20px;"> 
+<label for="email" data-error="Invalid Email Format" data-success="Valid Email"></label>
+</div>
+<div class="input-field">
+<input type="text" id="username" name="username" placeholder="Username"  required="required" style="border-radius:20px;">
+</div>
+<div class="input-field">
+<input type="password" name="password" placeholder="Password"  required="required" style="border-radius:20px;">
+</div>
+<input type="submit" name="signup" class="btn" value="Sign Up">
+</form>
+</div>
+</div>
+<!--Sign up and LOGIN area-->
+</div>
+
+<?php
+include "includes/footer.php";
+}
+else
+{
+   header("Location: login.php");
+}
+?>
